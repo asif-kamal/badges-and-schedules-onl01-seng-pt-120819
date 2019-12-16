472a01efdfa7412ee672457fb3a0dbc2d7bc3348
Asif Kamal <asif_kamal@hotmail.com>
2019-12-16 23:05:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array << "Hello, my name is #{name}."
+  end
+    new_array.each do |i|
+      puts i
+    end
+end
+  
+def assign_rooms(list)
+  room_assignments = []
+  list.each_with_index { |name, index| 
+  
+  room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!" 
+  }
+  room_assignments.each do |i|
+    puts i 
+  end
+end
+
+def printer
+  
+  batch_badge_creator(array).each {
+    |i| puts i 
+  }
+  assign_rooms(list).each { 
+    |i| puts i
+  }
+  
+end
